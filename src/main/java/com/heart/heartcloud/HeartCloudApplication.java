9139bfe5cbf7c5156684423f8bcaf90deec6dbb1
liHeart <lwf14@qq.com>
2019-03-12 11:23:22 +0800

    first commit

--- src/main/java/com/heart/heartcloud/HeartCloudApplication.java
+++ src/main/java/com/heart/heartcloud/HeartCloudApplication.java
@@ -1,15 +1,1 @@
-package com.heart.heartcloud;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-@MapperScan("com.heart.heartcloud.dao")
-public class HeartCloudApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HeartCloudApplication.class, args);
-    }
-
-}
+package com.heart.heartcloud;import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplication@MapperScan("com.heart.heartcloud.dao")public class HeartCloudApplication {    public static void main(String[] args) {        SpringApplication.run(HeartCloudApplication.class, args);    }}
