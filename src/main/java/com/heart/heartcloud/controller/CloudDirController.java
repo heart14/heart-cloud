80e13f070ae847674c034896645cd242abe0734a
heart14 <lwf14@qq.com>
2019-07-11 19:41:29 +0800

    HEART CLOUD

--- src/main/java/com/heart/heartcloud/controller/CloudDirController.java
+++ src/main/java/com/heart/heartcloud/controller/CloudDirController.java
@@ -1,6 +1,11 @@
 package com.heart.heartcloud.controller;
 
+import com.heart.heartcloud.domain.CloudDir;
+import com.heart.heartcloud.response.CloudResponse;
+import com.heart.heartcloud.service.CloudDirService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,4 +16,73 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/clouddir")
 @RestController
 public class CloudDirController {
+
+    @Autowired
+    private CloudDirService cloudDirService;
+
+    /**
+     * 新增文件夹
+     *
+     * @param cloudDir
+     * @return
+     */
+    @RequestMapping(name = "/save", method = RequestMethod.POST)
+    public CloudResponse saveCloudDir(CloudDir cloudDir) {
+        return null;
+    }
+
+    /**
+     * 删除文件夹（根据主键）
+     *
+     * @param cloudDirId
+     * @return
+     */
+    @RequestMapping(name = "/remove", method = RequestMethod.POST)
+    public CloudResponse removeCloudDir(Integer cloudDirId) {
+        return null;
+    }
+
+    /**
+     * 删除文件夹（根据用户ID）
+     *
+     * @param cloudUserId
+     * @return
+     */
+    @RequestMapping(name = "/removeAll", method = RequestMethod.POST)
+    public CloudResponse removeCloudDirs(Integer cloudUserId) {
+        return null;
+    }
+
+    /**
+     * 修改文件夹
+     *
+     * @param cloudDir
+     * @return
+     */
+    @RequestMapping(name = "/edit", method = RequestMethod.POST)
+    public CloudResponse editCloudDir(CloudDir cloudDir) {
+        return null;
+    }
+
+    /**
+     * 查询文件夹（根据主键）
+     *
+     * @param cloudDirId
+     * @return
+     */
+    @RequestMapping(name = "/find", method = RequestMethod.POST)
+    public CloudResponse findCloudDir(Integer cloudDirId) {
+        return null;
+    }
+
+    /**
+     * 查询文件夹（根据用户ID）
+     *
+     * @param cloudUserId
+     * @return
+     */
+    @RequestMapping(name = "/findAll", method = RequestMethod.POST)
+    public CloudResponse findCloudDirs(Integer cloudUserId) {
+        return null;
+    }
 }
