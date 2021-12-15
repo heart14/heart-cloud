b29f654702f9f341e53a3f7885d6f3e9c17f16d4
wfli <wfli@chinaums.com>
2021-12-15 17:03:01 +0800

    工具类

---
+++ src/main/java/com/heart/heartcloud/utils/mvndl/src/main/java/com/heart/mavendl/mvndl/common/Configs.java
@@ -0,0 +1,14 @@
+package com.heart.mavendl.mvndl.common;
+
+/**
+ * About:
+ * Other:
+ * Created: wfli on 2021/11/12 11:11.
+ * Editored:
+ */
+public class Configs {
+
+    public static final String ARCHIVES_ROOT_URL = "http://172.30.252.132:8081/nexus/content/groups/security/";
+    public static final String ARCHIVES_ROOT_PATH = "C:/Heart/Apache/repository2/";
+    public static final boolean IS_OVERRIDE = true;
+}
